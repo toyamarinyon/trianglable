38cf86bbb44b5ebc0cdd426a0950fdb45e80ad42
toyama satoshi <toyamarinyon@gmail.com>
2016-01-19 00:49:05 +0900

    Implements isosceles trianglable

--- trianglable.rb
+++ trianglable.rb
@@ -5,4 +5,8 @@ class Trianglable
     # 参考:http://mathtrain.jp/seiritu
     a+b>c && b+c>a && c+a>b
   end
+
+  def self.isosceles_trianglable(a, b, c)
+    trianglable(a,b,c) && (a==b || b==c || c==a)
+  end
 end
