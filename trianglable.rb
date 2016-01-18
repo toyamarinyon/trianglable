51c742790a6041fbe3a0b951b22a5e0bef4422fc
toyama satoshi <toyamarinyon@gmail.com>
2016-01-19 00:43:29 +0900

    Implements trianglable

--- trianglable.rb
+++ trianglable.rb
@@ -1,5 +1,8 @@
 class Trianglable
-  def self.valid_triangle(a, b, c)
-    true
+  def self.trianglable(a, b, c)
+    # 三角形の成立条件
+    # a+b>c && b+c>a && c+a>b でなければfalse
+    # 参考:http://mathtrain.jp/seiritu
+    a+b>c && b+c>a && c+a>b
   end
 end
